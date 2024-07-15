17d68ec06ddb842d1246d7488157a230766a412c
marce.ortiz11 <marceloortiz82@gmail.com>
2024-07-15 17:55:41 -0300

    primer commit

---
+++ src/public/main.js
@@ -0,0 +1,44 @@
+console.log('Js desde el cliente')
+const title = document.querySelector('#title-welcome');
+const chatBox = document.querySelector('#send');
+const socket = io();
+let user = '';
+
+Swal.fire({
+    title: 'ingrese Nickname',
+    input: 'text',
+    text: 'Identificacion para ingresar al chat',
+    allowOutsideClick: false,
+    inputValidator: (value) => {
+        return !value && 'ingresa un nick'
+    }
+}).then((result) => {
+    console.log(result.value)
+    user = result.value
+    title.innerText = 'Bienvenido al chat ' + user
+    socket.emit('newUser', { user });
+})
+
+
+chatBox.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        console.log('HOLA')
+        socket.emit('mensaje', { user, mensaje: event.target.value })
+        chatBox.value = ''
+    }
+})
+
+socket.on('conversacion', (data) => {
+    const contenedorChat = document.querySelector('#contenedor-chat');
+    contenedorChat.innerHTML = ''
+    data.forEach(chat => {
+        const div = document.createElement('div');
+        const nombre = document.createElement('p');
+        const mensaje = document.createElement('p');
+        nombre.innerText = chat.user + ': ';
+        mensaje.innerText = chat.mensaje;
+        div.appendChild(nombre);
+        div.appendChild(mensaje);
+        contenedorChat.appendChild(div);
+    })
+})
